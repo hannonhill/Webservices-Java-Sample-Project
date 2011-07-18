110a49aa30a8952ef842ea157ca81f3c3421af59
Bradley Wagner <bradley.wagner@hannonhill.com>
2011-07-18 16:51:32 -0400

    Changing testread to be a unit test

--- src/java/com/cms/webservices/TestRead.java
+++ src/java/com/cms/webservices/TestRead.java
@@ -1,20 +1,13 @@
 /*
  * Created on Apr 12, 2007 by Zach Bailey
- *
- * THE PROGRAM IS DISTRIBUTED IN THE HOPE THAT IT WILL BE USEFUL, BUT WITHOUT ANY WARRANTY. IT IS PROVIDED "AS IS" 
- * WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE 
- * PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR 
- * CORRECTION.
- * 
- * IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW THE AUTHOR WILL BE LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, 
- * INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE THE PROGRAM (INCLUDING BUT NOT LIMITED TO LOSS 
- * OF DATA OR DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE 
- * WITH ANY OTHER PROGRAMS), EVEN IF THE AUTHOR HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
  * 
  * Please feel free to distribute this code in any way, with or without this notice.
  */
 package com.cms.webservices;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 
 import com.hannonhill.www.ws.ns.AssetOperationService.AssetOperationHandler;
 import com.hannonhill.www.ws.ns.AssetOperationService.AssetOperationHandlerServiceLocator;
@@ -33,17 +26,19 @@ import com.hannonhill.www.ws.ns.AssetOperationService.ReadResult;
 public class TestRead
 {
     /**
-     * Main method for running as java console program. From inside
-     * Eclipse, run this file using alt+shift+x, j
+     * From inside Eclipse, run this test using:
      * 
-     * This should output something similar to: 
+     * - alt+shift+x, t (Windows)
+     * - option+command+x, t (Mac)
+     * 
+     * This should output something similar to:
      * 
      * "Got read result: com.hannonhill.www.ws.ns.AssetOperationService.ReadResult@145e16fe"
      * 
-     * @param args
      * @throws Exception
      */
-    public static void main(String[] args) throws Exception
+    @Test
+    public void testFolderRead() throws Exception
     {
         Identifier toRead = new Identifier();
         toRead.setId("ROOT");
@@ -53,13 +48,15 @@ public class TestRead
         read.setIdentifier(toRead);
 
         Authentication authentication = new Authentication();
-        authentication.setPassword("admin");
-        authentication.setUsername("admin");
+        authentication.setPassword("<REDACTED>");
+        authentication.setUsername("<REDACTED>");
 
         AssetOperationHandlerServiceLocator serviceLocator = new AssetOperationHandlerServiceLocator();
         AssetOperationHandler handler = serviceLocator.getAssetOperationService();
         ReadResult result = handler.read(authentication, toRead);
         System.out.println("Got read result: " + result);
         System.out.println("Read was " + (result.getSuccess().equals("true") ? "successful." : "unsuccessful. Error is: " + result.getMessage()));
+
+        assertTrue(result.getSuccess().equals("true") ? true : false);
     }
 }
