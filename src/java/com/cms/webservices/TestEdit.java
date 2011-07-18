31ed1bffb58a82dc5dbbacf9d073e03ac70211c3
Bradley Wagner <bradley.wagner@hannonhill.com>
2011-07-18 12:17:26 -0400

    Adding edit example

---
+++ src/java/com/cms/webservices/TestEdit.java
@@ -0,0 +1,75 @@
+/*
+ * Created on July 17, 2011 by Bradley Wagner
+ * 
+ * Please feel free to distribute this code in any way, with or without this notice.
+ */
+package com.cms.webservices;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.hannonhill.www.ws.ns.AssetOperationService.Asset;
+import com.hannonhill.www.ws.ns.AssetOperationService.AssetOperationHandler;
+import com.hannonhill.www.ws.ns.AssetOperationService.AssetOperationHandlerServiceLocator;
+import com.hannonhill.www.ws.ns.AssetOperationService.Authentication;
+import com.hannonhill.www.ws.ns.AssetOperationService.EntityTypeString;
+import com.hannonhill.www.ws.ns.AssetOperationService.Identifier;
+import com.hannonhill.www.ws.ns.AssetOperationService.OperationResult;
+import com.hannonhill.www.ws.ns.AssetOperationService.Page;
+import com.hannonhill.www.ws.ns.AssetOperationService.Path;
+import com.hannonhill.www.ws.ns.AssetOperationService.Read;
+import com.hannonhill.www.ws.ns.AssetOperationService.ReadResult;
+
+/**
+ * Tests the web services framework's edit operations
+ * 
+ * @author Bradley Wagner
+ */
+public class TestEdit
+{
+    /**
+     * Tests editing of a Page's title
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testPageTitleEdit() throws Exception
+    {
+        Identifier toRead = new Identifier();
+        Path path = new Path();
+
+        // set your page path and site name
+        path.setPath("/path/to/page");
+        path.setSiteName("<SITE-NAME>");
+        toRead.setPath(path);
+        toRead.setType(EntityTypeString.page);
+
+        Read read = new Read();
+        read.setIdentifier(toRead);
+
+        Authentication authentication = new Authentication();
+        authentication.setUsername("<REDACTED>");
+        authentication.setPassword("<REDACTED>");
+
+        // read the asset
+        AssetOperationHandlerServiceLocator serviceLocator = new AssetOperationHandlerServiceLocator();
+        AssetOperationHandler handler = serviceLocator.getAssetOperationService();
+        ReadResult result = handler.read(authentication, toRead);
+        assertTrue("Read was not successful", result.getSuccess().equals("true") ? true : false);
+
+        // edit the title
+        Asset readAsset = result.getAsset();
+        Page page = readAsset.getPage();
+        page.getMetadata().setTitle("new title");
+
+        OperationResult editResult = handler.edit(authentication, readAsset);
+        assertTrue("Edit was not successful", editResult.getSuccess().equals("true") ? true : false);
+
+        // re-read to verify title change
+        result = handler.read(authentication, toRead);
+        assertTrue("Read was not successful", result.getSuccess().equals("true") ? true : false);
+        assertEquals("Title was not changed", "new title", result.getAsset().getPage().getMetadata().getTitle());
+    }
+}
